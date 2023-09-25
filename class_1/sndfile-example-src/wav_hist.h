7da50317c3f7a4578767ce443dc5baea49241a45
goncalo <goncalolsilva@ua.pt>
2023-09-25 22:30:12 +0100

    added small fixes

--- class_1/sndfile-example-src/wav_hist.h
+++ class_1/sndfile-example-src/wav_hist.h
@@ -9,23 +9,33 @@
 class WAVHist {
   private:
 	// counts[value][counter]
-	std::vector<std::map<short, size_t>> counts;
+	std::vector<std::map<short, unsigned int>> counts;
 	size_t nChannels;
+	size_t binningFactor;
 
   public:
-	WAVHist(const SndfileHandle& sfh) {
+	WAVHist(const SndfileHandle& sfh, size_t bFactor = 1) {
 		if(sfh.channels() > 2)
 			throw std::invalid_argument("WAVHist can only handle 1 or 2 channels");
 
 		// Accounting for Mid and Side channels
 		counts.resize(sfh.channels() + 2);
 		nChannels = sfh.channels();
+		
+		if(bFactor % 2 != 0)
+			throw std::invalid_argument("Binning Factor must be even");
+
+		binningFactor = bFactor;
 	}
 
 	// The vector is in format: LR LR LR...
 	void update(const std::vector<short>& samples) {
+		/*
+			Constant value of bits to shift by: is (-2^15 + 2^15)/binningFactor ???? 
+				or max_min + max_max representation
+		*/
 		size_t n { };
-		for(size_t i = 0; i < samples.size(); i++){
+		for(int i = 0; i < samples.size(); i++){
 			counts[n++ % nChannels][samples[i]]++;
 			if(i > 0 && i%2 != 0){
 				short left = samples[i - 1];
