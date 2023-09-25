2af891db1152fd5f9e2ef784e414a0682103462a
goncalo <goncalolsilva@ua.pt>
2023-09-25 15:57:08 +0100

    fix on plot

--- class_1/sndfile-example-src/hist_create.m
+++ class_1/sndfile-example-src/hist_create.m
@@ -1,7 +1,6 @@
 %fileName = '../sndfile-example-bin/chMid.txt';
-fileName = '../sndfile-example-bin/chSide.txt';
+fileName = '../sndfile-example-bin/chMid.txt';
 T1 = readtable(fileName,'VariableNamingRule','preserve');
 
 figure(1);
-%plot(T1{:,1},T1{:,2})
-histogram(T1{:,1});
+plot(T1{:,1},T1{:,2})
