f2b0d73babc9e16a10d70a79f62b7b888a5c4cfd
SamuTheCoder <steixeira@ua.pt>
2023-09-30 19:58:16 +0100

    Creation of functions createBin and creatMidSide (output seems wrong)

--- class_1/sndfile-example-src/wav_hist.cpp
+++ class_1/sndfile-example-src/wav_hist.cpp
@@ -7,45 +7,58 @@ using namespace std;
 
 constexpr size_t FRAMES_BUFFER_SIZE = 65536; // Buffer for reading frames
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-	if(argc < 3) {
-		cerr << "Usage: " << argv[0] << " <input file> <channel>\n";
+	if (argc < 4)
+	{
+		cerr << "Usage: " << argv[0] << " <input file> <channel> <binSize>\n";
 		return 1;
 	}
 
-	SndfileHandle sndFile { argv[argc-2] };
-	if(sndFile.error()) {
+	SndfileHandle sndFile{argv[argc - 3]};
+	if (sndFile.error())
+	{
 		cerr << "Error: invalid input file\n";
 		return 1;
-    }
+	}
 
-	if((sndFile.format() & SF_FORMAT_TYPEMASK) != SF_FORMAT_WAV) {
+	if ((sndFile.format() & SF_FORMAT_TYPEMASK) != SF_FORMAT_WAV)
+	{
 		cerr << "Error: file is not in WAV format\n";
 		return 1;
 	}
 
-	if((sndFile.format() & SF_FORMAT_SUBMASK) != SF_FORMAT_PCM_16) {
+	if ((sndFile.format() & SF_FORMAT_SUBMASK) != SF_FORMAT_PCM_16)
+	{
 		cerr << "Error: file is not in PCM_16 format\n";
 		return 1;
 	}
 
+	size_t bFactor = stoi(argv[argc - 1]);
+	if (bFactor == 0)
+	{
+		cerr << "Error: bins should gather 2^k values (k > 0)\n";
+		return 1;
+	}
+
 	std::cout << "Number of channels: " << sndFile.channels() << '\n';
-	int channel { stoi(argv[argc-1]) };
-	if(channel == 1 && channel >= sndFile.channels() + 2) {
+	int channel{stoi(argv[argc - 2])};
+	if (channel == 1 && channel >= sndFile.channels() + 2)
+	{
 		cerr << "Error: invalid channel requested\n";
 		return 1;
 	}
 
 	size_t nFrames;
 	vector<short> samples(FRAMES_BUFFER_SIZE * sndFile.channels());
-	WAVHist hist { sndFile };
-	while((nFrames = sndFile.readf(samples.data(), FRAMES_BUFFER_SIZE))) {
+	WAVHist hist{sndFile, bFactor};
+	while ((nFrames = sndFile.readf(samples.data(), FRAMES_BUFFER_SIZE)))
+	{
 		samples.resize(nFrames * sndFile.channels());
-		hist.update(samples);
+		hist.update(samples, sndFile, FRAMES_BUFFER_SIZE);
 	}
 
 	hist.dump(channel);
 	return 0;
 }
-
--- class_1/sndfile-example-src/wav_hist.h
+++ class_1/sndfile-example-src/wav_hist.h
@@ -5,53 +5,101 @@
 #include <vector>
 #include <map>
 #include <sndfile.hh>
+#include <cmath>
 
-class WAVHist {
-  private:
+class WAVHist
+{
+private:
 	// counts[value][counter]
 	std::vector<std::map<short, unsigned int>> counts;
 	size_t nChannels;
 	size_t binningFactor;
 
-  public:
-	WAVHist(const SndfileHandle& sfh, size_t bFactor = 1) {
-		if(sfh.channels() > 2)
+public:
+	WAVHist(const SndfileHandle &sfh, size_t bFactor = 1)
+	{
+		if (sfh.channels() > 2)
 			throw std::invalid_argument("WAVHist can only handle 1 or 2 channels");
 
 		// Accounting for Mid and Side channels
 		counts.resize(sfh.channels() + 2);
 		nChannels = sfh.channels();
-		
-		if(bFactor % 2 != 0)
+
+		if (bFactor % 2 != 0)
 			throw std::invalid_argument("Binning Factor must be even");
 
 		binningFactor = bFactor;
 	}
 
+	std::vector<short> createBin(size_t binningFactor, const SndfileHandle &sfh, const std::vector<short> &samples, size_t FRAMES_BUFFER_SIZE)
+	{
+		std::vector<short> binnedSamples((FRAMES_BUFFER_SIZE * sfh.channels()));
+
+		short Lsum = 0, Rsum = 0;
+		int binnedSamplesCounter = 0;
+		for (size_t i = 0; i < samples.size(); i++)
+		{
+			if (i % 2 == 0 || i == 0)
+			{
+				Lsum += samples[i];
+			}
+			else
+			{
+				Rsum += samples[i];
+			}
+			int power = pow(2, binningFactor);
+			//When index reaches the last of the bin
+			//i.e. bFactor = 2 (2 lefts and 2 rights) ==> last index = 3, 7, 11, 15, ...
+			if (i % (power - 1) == 0)
+			{
+				binnedSamples[binnedSamplesCounter] = Lsum/(power/2);
+				binnedSamples[binnedSamplesCounter + 1] = Rsum/(power/2);
+				Lsum = Rsum = 0;
+				binnedSamplesCounter += 2;
+			}
+		}
+		return binnedSamples;
+	}
 	// The vector is in format: LR LR LR...
-	void update(const std::vector<short>& samples) {
+	void update(const std::vector<short> &samples, const SndfileHandle &sfh, size_t FRAMES_BUFFER_SIZE)
+	{
 		/*
-			Constant value of bits to shift by: is (-2^15 + 2^15)/binningFactor ???? 
+			Constant value of bits to shift by: is (-2^15 + 2^15)/binningFactor ????
 				or max_min + max_max representation
 		*/
-		size_t n { };
-		for(int i = 0; i < samples.size(); i++){
-			counts[n++ % nChannels][samples[i]]++;
-			if(i > 0 && i%2 != 0){
-				short left = samples[i - 1];
-				short right = samples[i];
-				counts[2][(left-right)/2]++; // Side
-				counts[3][(left+right)/2]++; // Mid
-			}
-		}	
+		if (binningFactor > 1)
+		{
+			std::vector<short> newSamples = createBin(binningFactor, sfh, samples, FRAMES_BUFFER_SIZE);
+			createMidSide(newSamples);
+		}
+		else
+		{
+			createMidSide(samples);
+		}
 	}
 
-	void dump(const size_t channel) const {
-		std::cout << "Value" << '\t' << "Count" <<  '\n';
-		for(auto [value, counter] : counts[channel])
-			std::cout << value << '\t' << counter << '\n';
+	void createMidSide(const std::vector<short> &samples)
+	{
+		size_t n{};
+		for (size_t i = 0; i < samples.size(); i++)
+		{
+			counts[n++ % nChannels][samples[i]]++;
+			if (i > 0 && i % 2 != 0)
+			{
+				short left = samples[i - 1];
+				short right = samples[i];
+				counts[2][(left - right) / 2]++; // Side
+				counts[3][(left + right) / 2]++; // Mid
+			}
+		}
+	}
+
+	void dump(const size_t channel) const
+	{
+		std::cout << "Value" << '\t' << "Count" << '\n';
+		for (auto [value, counter] : counts[channel])
+			std::cout << value << '\t' << counter <<'\n';
 	}
 };
 
 #endif
-
