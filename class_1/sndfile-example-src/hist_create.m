3d4862310b0d54c40e0bcc14bb5cd7393454e5bf
Gonçalo Silva <43342715+TheGoncaloSilva@users.noreply.github.com>
2023-10-16 23:21:12 +0100

    improved graphic plot

--- class_1/sndfile-example-src/hist_create.m
+++ class_1/sndfile-example-src/hist_create.m
@@ -1,6 +1,14 @@
 %fileName = '../sndfile-example-bin/chMid.txt';
 fileName = '../sndfile-example-bin/val.txt';
+name = 'Plot for the left channel';
+yLeg = 'Counter';
+xLeg = 'Value';
 T1 = readtable(fileName,'VariableNamingRule','preserve');
 
 figure(1);
-plot(T1{:,1},T1{:,2})
+plot(T1{:,1},T1{:,2});
+hold on;
+xlabel(xLeg);
+ylabel(yLeg);
+title(name);
+hold off;
