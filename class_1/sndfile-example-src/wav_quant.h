870c9e84ce6b34b86d2f862ccb70ecac716bf3ed
Tiago-AlvesUA <tiagojba9@ua.pt>
2023-10-28 00:46:32 +0000

    Correction on wav_cmp & wav_quant plots

--- class_1/sndfile-example-src/wav_quant.h
+++ class_1/sndfile-example-src/wav_quant.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <map>
 #include <sndfile.hh>
+#include <fstream>
 
 using namespace std;
 
@@ -12,13 +13,15 @@ class WAVQuant
 {
 private:
     vector<short> quantSamples;
+    int quantBits;
 
 public:
-    WAVQuant(){
+    WAVQuant(int quantBits){
         quantSamples.resize(0);
+        this->quantBits = quantBits;
     }
 
-    void quantize(const vector<short> &samples, int quantBits)
+    void quantize(const vector<short> &samples)
     {
         int cutBits = 16 - quantBits; 
         for (auto sample : samples){
@@ -31,6 +34,21 @@ public:
     void toFile(SndfileHandle sfhOut) {
         sfhOut.write(quantSamples.data(), quantSamples.size());
     }
+
+    void saveQuantizedSamples(const string &filename) {
+        ofstream outputFile(filename);
+        if (!outputFile.is_open()) {
+            cerr << "Error: Couldn't open the output file for writing." << endl;
+            return;
+        }
+
+        for (auto sample : quantSamples) {
+            outputFile << sample << endl;
+        }
+
+        outputFile.close();
+    }
+
 };
 
 #endif
