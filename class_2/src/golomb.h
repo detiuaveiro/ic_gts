7aaea9fb9868ece3a6ee083a38728c1318cc380a
Tiago-AlvesUA <tiagojba@ua.pt>
2023-11-11 15:21:12 +0000

    Decode function and Value interleaving implemented. Use of bitstream needs correction!

--- class_2/src/golomb.h
+++ class_2/src/golomb.h
@@ -14,10 +14,9 @@ class Golomb {
     private:
         int m;
         int approach; // (1) sign magnitude (2) positive/negative value interleaving
-        //FALTA BITSTREAM -> construtor golomb receber bitstream já aberta 
         BitStream& bitStream;
-
-
+        int total_bits; // vector to indicate number of bits to represent each integer
+        
         // Representar r de forma binária
         std::string getRemainderBinary(int r)
         {
@@ -25,12 +24,11 @@ class Golomb {
             
             if ((m & (m - 1)) == 0) // m é potência de 2
             {
-                //std::cout << "m : " << m << std::endl;
-                //std::cout << "log2(m) : " << log2(m) << std::endl;
-                for (int i = 0; i < log2(m); i++){ // log2(m) = nº bits para representar r
+                for (int i = 0; i < ceil(log2(m)); i++){ // log2(m) = nº bits para representar r
                     s = std::to_string(r % 2) + s;
                     r /= 2;
                 }
+                //remainder_bits.push_back(ceil(log2(m)));
             }
             else // truncar o código binário (m não é potência de 2)
             {
@@ -41,12 +39,14 @@ class Golomb {
                         s = std::to_string(r % 2) + s;
                         r /= 2;
                     }
+                    //remainder_bits.push_back(b - 1);
                 }else{ // encode os restantes valores de r usando b bits
                     r += pow(2, b) - m;
                     for (int i = 0; i < b; i++){
                         s = std::to_string(r % 2) + s;
                         r /= 2;
                     }
+                    //remainder_bits.push_back(b);
                 }
             }
 
@@ -54,19 +54,19 @@ class Golomb {
         }
 
     public:
-        Golomb(int m, BitStream& bitStream, int approach = 1): bitStream(bitStream) {
+        Golomb(int m, BitStream& bitStream, int approach = 1, int total_bits = 0): bitStream(bitStream) {
             this->m = m;
             this->approach = approach;
+            this->total_bits = total_bits;
             if (approach != 1 && approach != 2){
                 throw std::invalid_argument("Approach must be 1 - sign magnitude or 2 - positive/negative value interleaving");
             }
         }
 
-
         // Inteiro i é representado por 2 números: q e r
         // q é a parte inteira da divisão de n por m (parte unária)
         // r é o resto da divisão de n por m (parte binária)
-        std::string encode(int i)
+        void encode(int i)
         {
             std::string s = "";
             int q = 0;
@@ -96,20 +96,100 @@ class Golomb {
                     else
                         s = s + "0";
                 }
+                // converter s (binario) em inteiro
+                long int_binary_val = 0;
+                for (char c : s){
+                    int_binary_val = (int_binary_val << 1) + (c - '0');
+                }
+                bitStream.writeNBits(int_binary_val, s.length());
+                bitStream.~BitStream();
+                total_bits = s.length();
             }
-            else
-                // POSITIVE/NEGATIVE VALUE INTERLEAVING
-                return "";
+            else{
+                // VALUE INTERLEAVING 
 
-            
+                // if number is negative, multiply by 2 and subtract 1
+                if (i < 0) {
+                    i = -i;
+                    i = 2 * i - 1;
+                }else{
+                    i = 2 * i;
+                }
+                int q = floor(i / m);
+                int r = i % m;
 
-            return s;
+                for (int j = 0; j < q; j++)
+                    s += "1"; // representar q de forma unária
+                s += "0"; // separar q de r
 
-            // ESCREVER NO FICHEIRO DE SAIDA, USANDO WRITE N BITS
+                // representar r de forma binária
+                s += getRemainderBinary(r);
+
+                // converter s (binario) em inteiro
+                long int_binary_val = 0;
+                for (char c : s){
+                    int_binary_val = (int_binary_val << 1) + (c - '0');
+                }
+                bitStream.writeNBits(int_binary_val, s.length());
+                bitStream.~BitStream();
+                total_bits = s.length();
+            }
         }
 
         // Decode a sequence of bits
-        int decode(std::string s){
-            return 0;
+        int decode(){
+
+            long int_binary_val = bitStream.readNBits(total_bits);
+            std::string s = "";
+            for (int i = total_bits - 1; i >= 0; i--){
+                int bit = (int_binary_val >> i) & 1;
+                s += std::to_string(bit);
+            }
+
+            // Now that i have the string s, i can decode it
+            int value = 0;
+            int q = 0;
+            int r = 0;
+            for (int i = 0; i < s.length(); i++){
+                if (s[i] == '0'){
+                    q = i;
+                    break;
+                }
+            }
+            if (approach == 1){
+                // SIGN AND MAGNITUDE
+                     
+                // get the binary remainder
+                for (int i = q + 1; i < s.length()-1; i++){
+                    r = (r << 1) + (s[i] - '0');
+                }
+                value = q * m + r;
+                // last bit is the sign
+                if (s[s.length()-1] == '1')
+                    value = -value;
+                return value;
+            }else{
+                // VALUE INTERLEAVING
+
+                // get the binary remainder
+                for (int i = q + 1; i < s.length(); i++){
+                    r = (r << 1) + (s[i] - '0');
+                }
+                value = q * m + r;
+                // if number is odd, add 1 and divide by 2
+                if (value % 2 == 1){
+                    value++;
+                    value /= 2;
+                    value = -value;
+                }else{
+                    value /= 2;
+                }
+                return value;   
+            }
+        }
+
+
+        int getTotalBits(){
+            return total_bits;
         }
 };
--- class_2/src/test_golomb.cpp
+++ class_2/src/test_golomb.cpp
@@ -5,16 +5,63 @@
 using namespace std;
 
 int main(){
-    // m é potência de 2
-    BitStream bs = BitStream('w', "test_golomb.txt");
 
-    cout << "m=4 (Potência de 2)" << endl;
-    Golomb golomb1(4, bs); // m = 4
-    for (int i = 0; i <= 15; i++){
-        string s = golomb1.encode(i);
-        cout << i << " -> " << s << endl;
-    }
+    BitStream writer('w', "test_golomb.bin");    
+    Golomb golomb1(4, writer); 
+    golomb1.encode(2);
+    int bits_to_read = golomb1.getTotalBits();
+    
+    BitStream reader('r', "test_golomb.bin");
+    Golomb golomb2(4, reader, 1, bits_to_read);
+    int value = golomb2.decode();
+    cout << "value: " << value << endl;
 
+    BitStream writer0('w', "test_golomb0.bin");    
+    Golomb golomb0(4, writer0); 
+    golomb0.encode(-2);
+    bits_to_read = golomb0.getTotalBits();
+    
+    BitStream reader0('r', "test_golomb0.bin");
+    Golomb golomb0_2(4, reader0, 1, bits_to_read);
+    value = golomb0_2.decode();
+    cout << "value: " << value << endl;
+
+//--------------------------------------------------------------------------------//
+    // test value interleaving
+    BitStream writer2('w', "test_golomb2.bin");
+    Golomb golomb3(5, writer2, 2); // m = 5, interleaving
+    golomb3.encode(11); // Como 11 é positivo, vai ser dado encode de 2*11
+    bits_to_read = golomb3.getTotalBits();
+
+    BitStream reader2('r', "test_golomb2.bin");   
+    Golomb golomb4(5, reader2, 2, bits_to_read);
+    value = golomb4.decode();
+    cout << "value: " << value << endl;
+
+    BitStream writer3('w', "test_golomb3.bin");
+    Golomb golomb5(5, writer3, 2);
+    golomb5.encode(-11); // vai ser dado encode de 2*11 - 1
+    bits_to_read = golomb3.getTotalBits();
+    
+    BitStream reader3('r', "test_golomb3.bin");   
+    Golomb golomb6(5, reader3, 2, bits_to_read);
+    value = golomb6.decode();
+    cout << "value: " << value << endl;
+    
+    
+    // BitStream reader('r', "test_golomb.bin");
+    // Golomb golomb2(5, reader, 2, bits_to_read);
+    // int value = golomb2.decode();
+    // cout << "value: " << value << endl;
+
+
+    //Golomb golomb2(5, reader); 
+    // for (int i = 0; i <= 15; i++){
+    //     golomb2.decode();
+    //     //cout << i << " -> " << s << endl;
+    //     cout << endl;
+    // }
+    /*
     cout << endl;
     cout << "------------------------" << endl;
     cout << "m=5 (Não é potência de 2)" << endl;
@@ -31,6 +78,7 @@ int main(){
         string s = golomb2.encode(i);
         cout << i << " -> " << s << endl;
     }
+    */
 
     return 0;
 }
