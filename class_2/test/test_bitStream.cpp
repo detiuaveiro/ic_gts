a797097fae78010d109467bec27485426f1bf11b
goncalo <goncalolsilva@ua.pt>
2023-11-28 22:26:31 +0000

    improved test

--- class_2/test/test_bitStream.cpp
+++ class_2/test/test_bitStream.cpp
@@ -147,6 +147,7 @@ TEST(BitStream, testWGolombIntensive) {
 
     const int DEFAULT_M = 100;
     const int DEFAULT_P = 1;
+    const int GOLOMB_M = 4;
 
     BitStream writer('w', TEST_FILE_NAME);
 
@@ -158,7 +159,7 @@ TEST(BitStream, testWGolombIntensive) {
     writer.writeNBits(inF.lossy, 2);
     writer.writeNBits(inF.timesPlayed, 16);
 
-    Golomb golomb(4, writer);
+    Golomb golomb(GOLOMB_M, writer);
     std::list<int> values;
     // simulate blocks
     for (int i = 0; i <= 20; i++) {
@@ -191,7 +192,7 @@ TEST(BitStream, testWGolombIntensive) {
     EXPECT_EQ(inF.lossy, outF.lossy);
     EXPECT_EQ(inF.timesPlayed, outF.timesPlayed);
 
-    Golomb golomb2(4, reader);
+    Golomb golomb2(GOLOMB_M, reader);
     // Read blocks and check data
     for (int i = 0; i <= 20; i++) {  // number of blocks
         int m = reader.readNBits(8);
@@ -209,4 +210,80 @@ TEST(BitStream, testWGolombIntensive) {
     golomb2.~Golomb();
 
     remove(TEST_FILE_NAME);
+};
+
+TEST(BitStream, testWGolombIntensive_2) {
+
+    for (int GOLOMB_M = 2; GOLOMB_M <= 20; GOLOMB_M++) {
+
+        File inF;
+        inF.blockSize = 1024;
+        inF.nChannels = 2;
+        inF.sampleRate = 44100;
+        inF.nFrames = 100;
+        inF.quantizationBits = 8;
+        inF.lossy = true;
+        inF.timesPlayed = 12324;
+
+        const int DEFAULT_M = 100;
+        const int DEFAULT_P = 1;
+
+        BitStream writer('w', TEST_FILE_NAME);
+
+        writer.writeNBits(inF.blockSize, 16);
+        writer.writeNBits(inF.nChannels, 8);
+        writer.writeNBits(inF.sampleRate, 16);
+        writer.writeNBits(inF.nFrames, 32);
+        writer.writeNBits(inF.quantizationBits, 8);
+        writer.writeNBits(inF.lossy, 2);
+        writer.writeNBits(inF.timesPlayed, 16);
+
+        Golomb golomb(GOLOMB_M, writer);
+        // simulate blocks
+        for (int i = 0; i < 20; i++) {
+            writer.writeNBits(DEFAULT_M, 8);  // m value
+            writer.writeNBits(DEFAULT_P, 4);  // predictor value
+            for (int j = 0; j < 10; j++)
+                golomb.encode(10);
+        }
+
+        golomb.~Golomb();
+
+        File outF;
+        BitStream reader('r', TEST_FILE_NAME);
+
+        outF.blockSize = reader.readNBits(16);
+        outF.nChannels = reader.readNBits(8);
+        outF.sampleRate = reader.readNBits(16);
+        outF.nFrames = reader.readNBits(32);
+        outF.quantizationBits = reader.readNBits(8);
+        outF.lossy = reader.readNBits(2);
+        outF.timesPlayed = reader.readNBits(16);
+
+        EXPECT_EQ(inF.blockSize, outF.blockSize);
+        EXPECT_EQ(inF.nChannels, outF.nChannels);
+        EXPECT_EQ(inF.sampleRate, outF.sampleRate);
+        EXPECT_EQ(inF.nFrames, outF.nFrames);
+        EXPECT_EQ(inF.quantizationBits, outF.quantizationBits);
+        EXPECT_EQ(inF.lossy, outF.lossy);
+        EXPECT_EQ(inF.timesPlayed, outF.timesPlayed);
+
+        Golomb golomb2(GOLOMB_M, reader);
+        // Read blocks and check data
+        for (int i = 0; i < 20; i++) {  // number of blocks
+            int m = reader.readNBits(8);
+            int predictor = reader.readNBits(4);
+            EXPECT_EQ(m, DEFAULT_M);
+            EXPECT_EQ(predictor, DEFAULT_P);
+            // number of samples per block
+            for (int j = 0; j < 10; j++) {
+                int decoded = golomb2.decode();
+                EXPECT_EQ(decoded, 10);
+            }
+        }
+
+        golomb2.~Golomb();
+
+        remove(TEST_FILE_NAME);
+    }
 };
