bdfc5c911e669e6b9106269ea9098d8d8ecdd769
Gonçalo Silva <43342715+TheGoncaloSilva@users.noreply.github.com>
2023-12-03 23:57:24 +0000

    report and final touches

--- class_2/graphics/ex_4_distribution.m
+++ class_2/graphics/ex_4_distribution.m
@@ -2,8 +2,8 @@ fileName = '../bin/decoded_values_distribution.txt';
 T1 = readtable(fileName, 'VariableNamingRule', 'preserve');
 header = T1.Properties.VariableNames;
 name = 'Plot for the Error channel';
-yLeg = splitString{2};
-xLeg = 'Mono count';
+yLeg = 'Count';
+xLeg = 'Encoded Values';
 values = T1{:, 1};
 
 figure;
