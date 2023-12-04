d454ab072aa1a5a36c10bbfcc5280e9543715373
SamuTheCoder <steixeira@ua.pt>
2023-12-04 23:03:00 +0000

    Grayscale Converter class done, lacks google tests

---
+++ class_3/include/grayscale_converter.h
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+class GrayscaleConverter {
+   private:
+    std::fstream file;
+    Mat img;
+
+   public:
+    GrayscaleConverter(std::string fileName) {
+        this->file.open(fileName, std::fstream::in | std::fstream::binary);
+        this->img = imread(fileName);
+    }
+
+    bool IsGrayScale() {
+        //For grayscale, pixels must have the same value
+        for (int i = 0; i < img.rows; i++)
+        {
+            for (int j = 0; j < img.cols; j++)
+            {
+                Vec3b pixel = img.at<Vec3b>(i, j);
+                if(pixel[0] != pixel[1] != pixel[2]) return false;
+            }
+            
+        }
+
+        return true;
+    }
+
+    void ColorScaleToGrayScale() {
+        //Perform Color Scale change
+        if (IsGrayScale()) {
+            cout << "Image is already in greyscale\n";
+            return;
+        }
+
+        cvtColor(img, img, COLOR_BGR2GRAY);
+    }
+    ~GrayscaleConverter();
+};
