0ea9b7180a1c504df1e7041c50c3c8d96f40c69d
goncalo <goncalolsilva@ua.pt>
2023-12-11 14:47:05 +0000

    changes to image predictor

--- class_3/include/image_codec.h
+++ class_3/include/image_codec.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <tuple>
+#include <image_predictor.h>
 
 #define WRITER_FILE_NAME "outputStream.bin"
 #define DEFAULT_GOLOMB_M 4
@@ -21,45 +22,6 @@
 #define BITS_PREDICTOR 2
 
 using namespace std;
-
-/*
-##############################################################################
-###################           Predictor Class              ###################
-##############################################################################
-*/
-
-enum PREDICTOR_TYPE { AUTOMATIC, PREDICT1, PREDICT2, PREDICT3 };
-
-std::string get_type_string(PREDICTOR_TYPE type);
-
-class Predictor {
-   private:
-    /*! a1, a2 and a3 represent, a(n-1) a(n-2) and a(n-3) respectively
-            where n is the index of the predicted sample */
-    int predict1(int a1);
-    int predict2(int a1, int a2);
-    int predict3(int a1, int a2, int a3);
-
-    double calculate_entropy(PREDICTOR_TYPE type, std::vector<short>& samples);
-
-   public:
-    Predictor();
-    ~Predictor();
-
-    /*! 
-        Pass a set of samples/block and return the best predictor to be used 
-            (the one that resulted in less occupied space)
-    */
-    PREDICTOR_TYPE benchmark(std::vector<short>& samples);
-
-    /*!
-        Predict the next sample based on the type of the predictor and the
-            previous samples
-    */
-    int predict(PREDICTOR_TYPE type, std::vector<short>& samples, int index);
-
-    bool check_type(PREDICTOR_TYPE type);
-};
 
 /*
 ##############################################################################
