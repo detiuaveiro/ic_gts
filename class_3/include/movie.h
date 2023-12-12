edf32d6f88f341c56f89822791e393a903add6a3
SamuTheCoder <steixeira@ua.pt>
2023-12-12 15:50:55 +0000

    readFramesFromMovie algorithm is done, but still lacks an iteration through the frames mechanism

--- class_3/include/movie.h
+++ class_3/include/movie.h
@@ -4,25 +4,33 @@
 #include <string.h>
 #include <opencv2/opencv.hpp>
 
+typedef struct{
+    uint16_t chroma;
+    uint16_t width;
+    uint16_t height;
+    uint16_t fps;
+    int bytesPerFrame;
+}HeaderParameters;
+
 class Movie
 {
 private:
     int movieIndex;
-    vector<uint16_t> headerParameters;
+    HeaderParameters headerParameters;
 public:
     Movie(){
         this->movieIndex = 0;
     }
     ~Movie();
 
+    //get the parameters from an y4m file's header
     void getHeaderParameters(std::fstream movie){}
 
-    uint16_t getParameter(string line, size_t startPos){}
+    //auxiliar function used in getHeaderParameters to get one specific parameter
+    uint16_t getParameter(string line, size_t startPos, char parameterType){}
 
     //returns one frame of the video/movie
     vector<uint8_t> readFrameFromMovie(std::fstream movie){}
-
-    void getFrame(){}
 
     void createMovie(){}
 };
--- class_3/src/movie.cpp
+++ class_3/src/movie.cpp
@@ -2,53 +2,66 @@
 
 using namespace std;
 
-void Movie::getHeaderParameters(std::fstream movie){
-    if(!movie.is_open()){
+void Movie::getHeaderParameters(std::fstream movie) {
+    if (!movie.is_open()) {
         cerr << "Error: movie file is not open" << std::endl;
     }
 
     string line;
     getline(movie, line);
 
-    for (size_t i = 9; i < line.length(); i++)
-    {
-        switch (line[i])
-        {
-        case 'W':
-            headerParameters.push_back(getParameter(line, i));
-            break;
-        
-        case 'H':
-            headerParameters.push_back(getParameter(line, i));
-            break;
+    for (size_t i = 9; i < line.length(); i++) {
+        switch (line[i]) {
+            case 'W':
+                headerParameters.width = getParameter(line, i, 'W');
+                break;
 
-        case 'F':
-            headerParameters.push_back(getParameter(line, i));
-            break;
-        default:
-            break;
+            case 'H':
+                headerParameters.height = getParameter(line, i, 'H');
+                break;
+
+            case 'C':
+                headerParameters.chroma = getParameter(line, i, 'C');
+                break;
+            case 'F':
+                headerParameters.fps = getParameter(line, i, 'F');
+                break;
+            default:
+                break;
         }
     }
+
+    //for 4:2:0 chroma
+    headerParameters.bytesPerFrame = headerParameters.height * headerParameters.width * (3/2);
 }
-    
-uint16_t getParameter(string line, size_t startPos){
+
+uint16_t getParameter(string line, size_t startPos, char parameterType) {
     size_t endPos = line.find_first_of("\t", startPos);
-    
-    string parameter = line.substr(startPos+1, endPos - startPos);
+
+    string parameter;
+
+    //4:2:0 Chroma parameter is expected to have jpeg after the actual parameter
+    if (parameterType == 'C')
+        parameter = line.substr(startPos + 1, endPos - startPos - 4);
+    else
+        parameter = line.substr(startPos + 1, endPos - startPos);
 
     return stoi(parameter);
 }
 
-vector<uint8_t> Movie::readFrameFromMovie(std::fstream movie){
-    if(!movie.is_open()){
+//LACKS ITERATION-LIKE WAY OF GETTING FRAMES
+vector<uint8_t> Movie::readFrameFromMovie(std::fstream movie) {
+    if (!movie.is_open()) {
         cerr << "Error: movie file is not open" << std::endl;
     }
 
     string line;
 
-    while (getline(movie, line))
-    {
-        
-    }
-    
+    vector<uint8_t> frameData;
+
+    //frameData.data() returns pointer to data inside vector
+    //reinterpret_cast<char*> used for the * to be interpretated as a char* because readsome requires it to be
+    movie.readsome(reinterpret_cast<char*>(frameData.data()), headerParameters.bytesPerFrame);
+
+    return frameData;
 }
